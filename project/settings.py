d53044c8914581a8b75cff8443bdb93dea20aaf8
Kirill <dawiduk2013@yandex.ru>
2026-05-09 20:57:03 +0500

    Update settings.py

--- project/settings.py
+++ project/settings.py
@@ -1,21 +1,24 @@
 import os
+from dotenv import load_dotenv
+load_dotenv()
+
 
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'HOST': '',
-        'PORT': '',
-        'NAME': '',
-        'USER': '',
-        'PASSWORD': '',
+        'HOST': os.getenv('SECURITY_DB_HOST'),
+        'PORT': os.getenv('SECURITY_DB_PORT'),
+        'NAME': os.getenv('SECURITY_DB_NAME'),
+        'USER': os.getenv('SECURITY_DB_USER'),
+        'PASSWORD': os.getenv('SECURITY_DB_PASSWORD'),
     }
 }
 
 INSTALLED_APPS = ['datacenter']
 
-SECRET_KEY = 'REPLACE_ME'
+SECRET_KEY = os.getenv('SECURITY_SECRET_KEY')
 
-DEBUG = True
+DEBUG = False
 
 ROOT_URLCONF = 'project.urls'
 
