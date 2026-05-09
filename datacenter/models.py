7a6733b2aa591071fbc07b95337088a9e82cfd4f
Kirill <dawiduk2013@yandex.ru>
2026-05-09 20:54:29 +0500

    Update models.py

--- datacenter/models.py
+++ datacenter/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.utils.timezone import localtime
 
 
 class Passcard(models.Model):
@@ -28,3 +29,20 @@ class Visit(models.Model):
                 if self.leaved_at else 'not leaved'
             )
         )
+
+    def get_duration(self):
+        leaving_time = localtime(value=self.leaved_at, timezone=None)
+        entering_time = localtime(value=self.entered_at, timezone=None)
+        delta = leaving_time - entering_time
+        delta_total_seconds = int(delta.total_seconds())
+        return delta_total_seconds
+
+    def is_visit_long(self, minutes=60):
+        return self.get_duration() > minutes * 60
+
+
+def format_duration(duration):
+    delta_hours = duration//3600
+    delta_minutes = (duration % 3600) // 60
+    delta_seconds = (duration % 3600) % 60
+    return f'{delta_hours:02d}:{delta_minutes:02d}:{delta_seconds:02d}'
